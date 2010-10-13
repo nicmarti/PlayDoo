59eda8b2237a2e92f1cf4e72c10b76260d170758
Nicolas MARTIGNOLE <nicolas@touilleur-express.fr>
2010-10-13 21:58:17 +0200

    step1 show model in view

--- todoApp/app/controllers/Application.java
+++ todoApp/app/controllers/Application.java
@@ -1,11 +1,16 @@
 package controllers;
 
+import models.Todo;
 import play.mvc.*;
+
+import java.util.List;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        List<Todo> listOfTodos=Todo.findAll() ;
+
+        render(listOfTodos);
     }
 
 }
