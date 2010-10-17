8fce25ce83d9184431751e772ad1dc0418015d72
Nicolas MARTIGNOLE <nicolas@touilleur-express.fr>
2010-10-17 19:44:44 +0200

    New look and feel and implemented the save action

--- todoApp/app/controllers/Application.java
+++ todoApp/app/controllers/Application.java
@@ -9,24 +9,38 @@ import java.util.List;
 public class Application extends Controller {
 
     public static void index() {
-        List<Todo> listOfTodos=Todo.findAll() ;
+        List<Todo> listOfTodos = Todo.findAll();
 
         render(listOfTodos);
     }
 
-    public static void create(String title, Date dueDate){
-        Todo todo=new Todo(title,dueDate);
+    public static void create(String title, Date dueDate) {
+        Todo todo = new Todo(title, dueDate);
         todo.save();
         index();
     }
 
-    public static void show(Long id,String title){
-        Todo event=Todo.findById(id);
+    public static void show(Long id, String title) {
+        Todo event = Todo.findById(id);
         render(event);
     }
 
-    public static void form(Long id){
-        render();
+    public static void form(Long id) {
+        if (id == null) {
+            render();
+        }
+        Todo todo = Todo.findById(id);
+        render(todo);
+    }
+
+    public static void save(Todo todo) {
+        if (validation.hasErrors()) {
+            if (request.isAjax()) error("Invalid value");
+            render("@form", todo);
+        }
+        todo.save();
+        index();
+
     }
 
 }
